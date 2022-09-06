9f2c42b65bea3afc78ba91dbb28b60a8b3d9ce61
tayhooser <84413327+tayhooser@users.noreply.github.com>
2022-09-06 08:53:33 -0700

    image support

---
+++ lab3.php
@@ -0,0 +1,12 @@
+ <?php
+$param = $_GET['param'];
+echo "param is: " . $param . "<br>";
+if ($param == "hello") {
+    echo "Hello student!<br>";
+} else {
+    $num = $param*2+3;
+    echo "number:" . $num . "<br>";
+}
+echo "<br>";
+echo "<img src=\"my_diagram3.gif\"  style="width:800px;height:auto;"/>";
+?> 
